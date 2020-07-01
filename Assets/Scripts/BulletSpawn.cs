6e77c6767e78c39e52f49660a266297b13a19230
rlwns07 <66991670+rlwns07@users.noreply.github.com>
2020-07-01 19:17:27 +0900

    Revert "0701"
    This reverts commit 94a50edc827013efba39a066b2cb7e753ab931a9.

--- Assets/Scripts/BulletSpawn.cs
+++ Assets/Scripts/BulletSpawn.cs
@@ -6,6 +6,7 @@ public class BulletSpawn : MonoBehaviour
 {
     public float delay = 0.5f;      //총알 삭제 시간
 
+
     void Update()
     {
         //발사 후 일정 시간 지나면 총알 삭제 처리
--- Assets/Scripts/GunControll.cs
+++ Assets/Scripts/GunControll.cs
@@ -1,38 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;                   // 총알 개수 UI 설정을 위한 네임스페이스
 
 public class GunControll : MonoBehaviour
 {
-    [Header("총 연사속도 조정")]
-    public float fireRate;
-
-    [Header("총알 개수")]
-    public int bulletCount;
-    public int maxBulletCount;
-
-    [Header("애니메이터")]
-    public Animator animator;
-
-    [Header("총알 스피드")]
-    public float speed;
-
-    [Header("총알 발사 사운드")]
-    public string sound_Fire;
-
     [SerializeField] ParticleSystem ps_MuzzleFlash = null;     //적용할 파티클 효과
     [SerializeField] GameObject go_Bullet_Prefab = null;       //적용 대상 총알 오브젝트
     [SerializeField] float bulletSpeed = 0f;                 //발사 속도
 
-    [SerializeField] Text txt_CurrentGunBullet;             // 총알 개수를 나타내는 텍스트 UI
-
     // Start is called before the first frame update
     void Start()
     {
-        
     }
     
+
     // Update is called once per frame
     void Update()
     {
