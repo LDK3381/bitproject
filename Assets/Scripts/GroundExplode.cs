ff7cde1c66afa006bb1dac41ea71f0f7ec221ac9
Goris418 <dankkh418@naver.com>
2020-07-22 04:32:56 +0900

    0722_Player1
    폭탄 범위 애니메이션 버그 수정

--- Assets/Scripts/GroundExplode.cs
+++ Assets/Scripts/GroundExplode.cs
@@ -6,16 +6,18 @@ public class GroundExplode : MonoBehaviour
 {
     private Animation anima = null;
 
-    private void Start()
+    void Start()
     {
         anima = GetComponent<Animation>();
     }
+
 
     public void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.tag == "Bomb")
         {
             Destroy(gameObject, 2);     //폭탄이 놓인 곳의 발판을 2초 후에 소멸
+
         }
     }
 
@@ -24,14 +26,6 @@ public class GroundExplode : MonoBehaviour
         if (other.gameObject.tag == "BombCheck")
         {
             anima.Play("GroundCount");
-
-            float time = 0f;
-            time += Time.deltaTime;
-            if(time > 2f)
-            {
-                Debug.Log("애니메이션 중지");
-                anima.Stop("GroundCount");
-            }
         }
     }
 }
