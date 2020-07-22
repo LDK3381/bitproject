6a5ac7cad86709867a744daeb7200972fd35e159
anMois <66979888+anMois@users.noreply.github.com>
2020-07-22 14:36:56 +0900

    0722_2
    pm02h36m

---
+++ Assets/Scripts/Multi/MtPickUpItem.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MtPickUpItem : MonoBehaviour
+{
+    [SerializeField] Gun[] guns = null;
+
+    public MtGunController theGC;
+
+    const int NOMAL_GUN = 0;
+
+    // 아이템과 충돌
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.CompareTag("Item"))
+        {
+            Item item = other.GetComponent<Item>();
+
+            int extra = 0;
+
+            // 일반 총알을 획득했을 떄
+            if (item.itemType == ItemType.NomalGun_Bullet)
+            {
+                SoundManager.instance.PlaySE("Bullet");
+                extra = item.itemBullet;
+                guns[NOMAL_GUN].bulletCount += extra;
+                theGC.BulletUiSetting();
+            }
+
+            string message = "+" + extra;
+
+            //FloatingTextManager.instance.CreateFloatingText(other.transform.position, message);
+
+            Destroy(other.gameObject);
+        }
+    }
+}
