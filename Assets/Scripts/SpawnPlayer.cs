a978173064fb9eb9eec76ddbc36a8c151051889d
LDK3381 <66936254+LDK3381@users.noreply.github.com>
2020-07-27 13:02:13 +0900

    0727_UI
    캐릭터 선택, 카운트 제작

--- Assets/Scripts/SpawnPlayer.cs
+++ Assets/Scripts/SpawnPlayer.cs
@@ -10,29 +10,19 @@ public class SpawnPlayer : MonoBehaviour
     public GameObject HPUI;     //HP UI
     public GameObject Choice;   //캐릭터 선택 UI
     public GameObject[] Point;  //캐릭터 스폰 위치
-
-    public Text timeText = null;
-    private float time;
-    [SerializeField] string[] nickName = null;
-
-    private void Awake()
-    {
-        time = 10f;
-    }
+    public MtCount mtCount;     //캐릭터 선택 제한 시간
+    private string nick;        //선택한 캐릭터 이름
+    private bool isCheck;       //시간 내에 캐릭터를 선택하였는가?
 
     private void Start()
     {
+        isCheck = false;
         HPUI.SetActive(false);
     }
 
-    void Update()
+    private void Update()
     {
-        if (time > 0)
-            time -= Time.deltaTime;
-
-        timeText.text = Mathf.Ceil(time).ToString();
-        if (time == 0f)
-            OnCreate(nickName[Random.Range(0, 4)]);
+        NotChoiceCreate();
     }
 
     //스폰 위치 체크
@@ -51,22 +41,48 @@ public class SpawnPlayer : MonoBehaviour
         }
     }
 
+    private void NotChoiceCreate()  //버튼 선택 안하면.
+    {
+        if (mtCount.flag == false && isCheck == false)
+        {
+            RandomCreate();
+            Destroy(gameObject);    //무한 스폰 방지
+        }
+    }
+
+    private void ChoiceCreate(string Nickname)   //버튼 선택시.
+    {
+        nick = Nickname;
+        isCheck = true;
+        StartCoroutine("CreatePlayer");
+    }
+    IEnumerator CreatePlayer()
+    {
+        yield return new WaitForSeconds(mtCount.timeCost);  //남은시간
+        OnCreate(nick);
+        Destroy(gameObject);
+    }
+
+    public void RandomCreate()
+    {
+        switch (Random.Range(0, 4))
+        {
+            case 0: nick = "Kitchen"; break;
+            case 1: nick = "Kitchen"; break;
+            case 2: nick = "Kitchen"; break;
+            case 3: nick = "Kitchen"; break;
+            default:
+                break;
+        }
+        OnCreate(nick);
+    }
+
     public void OnCreate(string Nickname)
     {
-        try
-        {
-            if(time == 0)
-            {
-                PhotonNetwork.Instantiate(Nickname, new Vector3(0.375f, 0.6f, 0.375f), Quaternion.identity);
-                Choice.SetActive(false);
-                HPUI.SetActive(true);
-                StartCoroutine("DestroyBullet");
-            }
-        }
-        catch
-        {
-            Debug.Log("반복");
-        }
+        PhotonNetwork.Instantiate(Nickname, new Vector3(0.375f, 0.6f, 0.375f), Quaternion.identity);
+        Choice.SetActive(false);
+        HPUI.SetActive(true);
+        StartCoroutine("DestroyBullet");
     }
 
     IEnumerator DestroyBullet()
@@ -77,5 +93,4 @@ public class SpawnPlayer : MonoBehaviour
             GO.GetComponent<PhotonView>().RPC("DestroyRPC", RpcTarget.All);
         }
     }
-
 }
