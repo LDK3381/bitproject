92f69b99fea135705f679aad13b2ff21ebd21737
Goris418 <dankkh418@naver.com>
2020-08-13 20:38:07 +0900

    0813_Master2
    1. 자신의 샷건으로 맞는 버그 수정
    2. 좌측 상단 큰 체력바 안깎이는 버그 수정
    3. 쏠 때마다 무기 깜빡이는 버그 수정(애니메이션 오류)

--- Assets/Scripts/Weapon/FireGun_Bullet.cs
+++ Assets/Scripts/Weapon/FireGun_Bullet.cs
@@ -12,7 +12,7 @@ public class FireGun_Bullet : MonoBehaviourPun
         try
         {
             flag = PlayerInRoom();
-            Destroy(gameObject, 3.0f);
+            Destroy(transform.parent.gameObject, 3.0f);
         }
         catch (System.Exception ex)
         {
@@ -53,8 +53,9 @@ public class FireGun_Bullet : MonoBehaviourPun
         {
             Debug.Log("샷건으로 캐릭터 피격");
             DecreaseHp(other);
+            //other.transform.GetComponent<StatusManager>().SgDecreaseHp(1);
         }
-        //폭탄 범위 내 벽이 있으면 벽 폭파
+        // 샷건으로 벽 폭파
         if (other.transform.CompareTag("BreakableWall"))
         {
             Debug.Log("샷건으로 벽 파괴");
